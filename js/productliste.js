5edb25cd44333ff8ded24649a5b5859c52e618c2
ZOEE-max <zoewyke@gmail.com>
2025-02-03 12:38:00 +0100

    ændringer

--- js/productliste.js
+++ js/productliste.js
@@ -4,19 +4,22 @@ fetch(`https://kea-alt-del.dk/t7/api/products/`)
   .then((response) => response.json())
   .then((data) => showList(data));
 
-function showList(products) {
-  console.log(products);
-  let markup = "";
-  products
-    .map((product) => {
-      markup += `<div class="product_list_container">
-
+function showList(data) {
+  console.log(data);
+  //   let markup = "";
+  //  products
+  //     .map((product) => {
+  //       markup +=
+  const markup = data
+    .map(
+      (product) =>
+        `<div class="product_list_container">
             <a href="produkt.html" class="product_card out-of-stock">
                 <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="Produkt 1">
                 <h3>${product.productdisplayname}</h3>
                 <p>${product.price}</p>
-            </div></a> `;
-    })
+            </div></a> `
+    )
 
     .join("");
   console.log(markup);
